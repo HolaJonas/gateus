656853f187c2b91c29d8774053f256c9497752ef
HolaJonas <jokil04122@gmail.com>
2025-12-13 00:01:43 +0100

    feat: Added basic header
    Added a basic header containing placeholder elements for future Implementations. See FIGMA

--- gateus/src/App.tsx
+++ gateus/src/App.tsx
@@ -1,7 +1,14 @@
+import Header from "./components/header/Header"
+
 
 function App() {
   return (
-    <div className="bg-red-100 min-h-screen flex items-center justify-center">Test</div>
+    <div className="bg-red-100 min-h-screen flex">
+      <Header/>
+      <div className="">
+
+      </div>
+    </div>
   );
 }
 
---
+++ gateus/src/components/header/Header.tsx
@@ -0,0 +1,30 @@
+/**
+ * The Header-component for the main screen.
+ * Displays buttons within a list centered in the header-bar.
+ *
+ * @returns {JSX.Element} Header bar with according buttons.
+ */
+
+function Header() {
+  return (
+    <div className="w-full h-16 bg-red-600 shadow border-red-400 border relative flex items-center">
+      Logo
+      <div className="absolute left-1/2 -translate-x-1/2 flex gap-10 items-center">
+        <a
+          href=""
+          className="block py-2 px-3 text-gray-700 rounded md:bg-transparent md:text-primary-700 md:p-0 dark:text-white hover:text-blue-600"
+        >
+          1
+        </a>
+        <a
+          href=""
+          className="block py-2 px-3 text-gray-700 rounded md:bg-transparent md:text-primary-700 md:p-0 dark:text-white hover:text-blue-600"
+        >
+          2
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
